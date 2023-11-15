dc3e6e5ff6d8ac9be349c3e3a45e8fb0e12885fa
dan <daniilbychkovit@gmail.com>
2023-11-15 15:59:47 +0300

    add funcs

--- test_task/drevo_menu/models.py
+++ test_task/drevo_menu/models.py
@@ -4,10 +4,6 @@ from django.db import models
 class MenuDrevo(models.Model):
     menu_title = models.CharField(max_length=50, blank=True, null=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
-    
-    def get_parent(self):
-        if self.parent:
-            return self.parent.pk
-        
+
     def __str__(self):
         return self.menu_title
--- test_task/drevo_menu/templatetags/drevo_menu_extras.py
+++ test_task/drevo_menu/templatetags/drevo_menu_extras.py
@@ -5,6 +5,16 @@ from drevo_menu.models import MenuDrevo
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def draw_menu(context):
-    print(context['request'].path)
-    return  MenuDrevo.objects.all()
+def draw_menu(context, name_menu):
+    all_categories = MenuDrevo.objects.all()
+    main_menu = list(filter(lambda category: category.menu_title == name_menu, all_categories))[0]
+    main_menu.parent = None
+    children_categories = {}
+    #--wrapper
+    def tree(main_menu, all_categories):
+        children_categories[main_menu] = list(filter(lambda category: category.parent == main_menu, all_categories))
+
+        return children_categories
+    #--Wraper
+
+    return tree(main_menu, all_categories)
