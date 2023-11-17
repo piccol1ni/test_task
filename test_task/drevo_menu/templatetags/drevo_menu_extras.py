c2f5d4bee0ce1f645affca0010421c441c45e53a
user <user>
2023-11-17 07:59:04 +0300

    add all

--- test_task/drevo_menu/templatetags/drevo_menu_extras.py
+++ test_task/drevo_menu/templatetags/drevo_menu_extras.py
@@ -8,13 +8,21 @@ register = template.Library()
 def draw_menu(context, name_menu):
     all_categories = MenuDrevo.objects.all()
     main_menu = list(filter(lambda category: category.menu_title == name_menu, all_categories))[0]
-    main_menu.parent = None
+    menu_categories = list(filter(lambda category: category.parent == main_menu, all_categories))
     children_categories = {}
-    #--wrapper
-    def tree(main_menu, all_categories):
-        children_categories[main_menu] = list(filter(lambda category: category.parent == main_menu, all_categories))
+
+    def tree(main_menu):
+        if menu_categories == []:
+            return main_menu
+        else:
+            for category in menu_categories:
+                return tree(category)
+        
+
+        
 
         return children_categories
     #--Wraper
 
     return tree(main_menu, all_categories)
+
