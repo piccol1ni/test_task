896bcb8fca10b581843e19ab820e081d53603518
user <user>
2023-11-19 21:14:00 +0300

    add new

--- test_task/drevo_menu/templatetags/drevo_menu_extras.py
+++ test_task/drevo_menu/templatetags/drevo_menu_extras.py
@@ -4,25 +4,17 @@ from drevo_menu.models import MenuDrevo
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
+@register.inclusion_tag('drevo_menu/draw_menu.html', takes_context=True)
 def draw_menu(context, name_menu):
     all_categories = MenuDrevo.objects.all()
     main_menu = list(filter(lambda category: category.menu_title == name_menu, all_categories))[0]
-    menu_categories = list(filter(lambda category: category.parent == main_menu, all_categories))
     children_categories = {}
 
-    def tree(main_menu):
-        if menu_categories == []:
-            return main_menu
-        else:
-            for category in menu_categories:
-                return tree(category)
-        
+    for category in all_categories:
+        children_categories[category] = list(filter(lambda categori: categori.parent == category, all_categories))
 
-        
-
-        return children_categories
-    #--Wraper
-
-    return tree(main_menu, all_categories)
+    return {
+        'children_categories': children_categories,
+        'main_menu': main_menu,
+    }
 
